7992ede7b5b9e9887a40ae403002830ddb466600
mozzi0131 <sue03094@gmail.com>
2020-09-30 17:41:37 +0900

    Write a skeleton for the engine

---
+++ gameobj/GameObject/gameobj.h
@@ -0,0 +1,9 @@
+// The definition of the Game object
+// image, size, position
+// the object's shape will be a square
+// controller (moving the pos by keyboard controller)
+
+#ifndef GAMEOBJECT_GAMEOBJ_H
+#define GAMEOBJECT_GAMEOBJ_H
+
+#endif //GAMEOBJECT_GAMEOBJ_H
---
+++ rawobject/SoundObj/soundobj.cpp
@@ -0,0 +1,1 @@
+#include "soundobj.h"
