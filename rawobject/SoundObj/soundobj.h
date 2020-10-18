68714221abd40e0b9c4d3b30e666a0da8c50cf40
Soohyun Lee <sue03094@gmail.com>
2020-10-18 11:18:25 +0900

    Change the directory's hierarchy
    - Rename the directory
    - add meson.build to each dir

--- rawobject/SoundObj/soundobj.h
+++
@@ -1,9 +0,0 @@
-// It will be great if I could use gstreamer in this project
-// (because I already know how to use it)
-// But maybe I have to use OpenAL either
-// just play the bgm, or short sound effect
-
-#ifndef RAWOBJECT_SOUNDOBJ_SOUNDOBJ_H
-#define RAWOBJECT_SOUNDOBJ_SOUNDOBJ_H
-
-#endif //RAWOBJECT_SOUNDOBJ_SOUNDOBJ_H
