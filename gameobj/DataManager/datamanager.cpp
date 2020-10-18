9214a1cad3bcbcf9921012a30f1c0a9ce9b46f23
Soohyun Lee <sue03094@gmail.com>
2020-10-18 11:18:25 +0900

    Change the directory's hierarchy
    - Rename the directory
    - add meson.build to each dir

--- gameobj/DataManager/datamanager.cpp
+++
@@ -1,1 +0,0 @@
-#include "datamanager.h"
--- gameobj/GameObject/gameobj.h
+++
@@ -1,9 +0,0 @@
-// The definition of the Game object
-// image, size, position
-// the object's shape will be a square
-// controller (moving the pos by keyboard controller)
-
-#ifndef GAMEOBJECT_GAMEOBJ_H
-#define GAMEOBJECT_GAMEOBJ_H
-
-#endif //GAMEOBJECT_GAMEOBJ_H
