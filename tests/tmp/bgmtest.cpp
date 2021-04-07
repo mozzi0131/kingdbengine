61dff4f403f7fa21d7408b39f970de7597cfea6f
Soohyun Lee <sue03094@gmail.com>
2021-04-08 00:21:48 +0900

    update test

--- tests/tmp/bgmtest.cpp
+++ tests/tmp/bgmtest.cpp
@@ -20,28 +20,15 @@ int main() {
   BgmPipeline *obj = new BgmPipeline();
   obj->SetBGM(mp3dir);
 
-  // std::string mp3dir2 =
-  // "C:\\Users\\User\\Desktop\\workspace\\kingdbengine\\tests\\resource\\freejazz.mp3";
-  // AudioObj *obj2 = new AudioObj(mp3dir2);
-
-  // obj2->Play();
   sleep(10);
 
   obj->SetBGM(newMp3dir);
 
   sleep(10);
-  obj->SetBGM(mp3dir);
 
-  sleep(10);
-  obj->Pause();
-  // obj2->Pause();
-
-  // sleep(5);
   obj->Unload();
-  // obj2->Unload();
 
   delete obj;
-  // delete obj2;
 
   return 0;
 }
